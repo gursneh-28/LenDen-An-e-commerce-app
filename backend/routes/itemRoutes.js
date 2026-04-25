0678bef697dc954a7efd9aea5bcd0b1cdcc2abd8
gursneh-28 <gursnehkaur1@gmail.com>
2026-04-25 17:04:38 +0530

    update

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 const itemController = require("../controllers/itemController");
 const verifyToken = require("../middleware/authMiddleware");
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: require("os").tmpdir() });
 
 // Use upload.array("images", 5) to accept up to 5 images under the field name "images"
 router.post("/upload", verifyToken, upload.array("images", 5), itemController.uploadItem);
